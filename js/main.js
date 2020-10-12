2c0551f34b787c7e4af0860a4cc658bf0dfb7d1e
David Adarkwah <72516754+Adark-Amal@users.noreply.github.com>
2020-10-12 12:47:32 +0000

    Changed an event and printed a sentence to the screen

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-  $('.box').on('click','.box-button', function () {
-    $(this).parent().toggleClass('highlight');
+  $('#select-menu').on('change', function () {
+    let name = $('#select-menu option:selected').text();
+    let distance = $('#select-menu option:selected').val();
+    let price = $('#select-menu option:selected').data('price');
+    
+    console.log(price);
+    
+    if (distance) {
+      $('#feedback-message').text('You are signing up for a ' + name + 
+    ', which costs ' + price + ', to a distance of ' 
+    + distance + 'km');
+    } else {
+      $('#feedback-message').empty();
+    }
   });
 });
-
-// You can use $(this).toggleClass('hightlight')
