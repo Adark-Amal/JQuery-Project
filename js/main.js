fa23fc2c8be593384d9138b396a4fd6ebfdf2957
David Adarkwah <72516754+Adark-Amal@users.noreply.github.com>
2020-10-13 09:21:34 +0000

    Added an event to the button remove to remove unwanted carts

--- js/main.js
+++ js/main.js
@@ -9,13 +9,18 @@ $(document).ready(function () {
     html += '<img src="assets/image.webp" alt="Image">';
     html += '<div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quaerat </div>';
     html += '<div class="price">499</div>';
-    html += '<button>Add to Cart</button>';
+    html += '<button class="item-add">Add to Cart</button>';
+    html += '<button class="item-remove">Remove</button>';
     html += '<br>';
     html += '<a href="#">More info</a>';
     html += '<div class="more-info">Lorem ipsum dolor sit amet consectetur, omnis corporis id excepturi </div>';
     html += '</div>';
 
     $('#container').prepend(html);
+  });
+
+  $('#container').on('click', '.item-remove', function() {
+    $(this).parent().remove();
   });
 });
 // You can use $(this).toggleClass('hightlight')
