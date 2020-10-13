885371428cbcdb07d23454f917a1015adc5bdfd2
David Adarkwah <72516754+Adark-Amal@users.noreply.github.com>
2020-10-13 08:50:35 +0000

    Used jquery to add items dynamically

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
- $('#input-name').on('keyup', function () {
-    let name = $(this).val();
-    $('#feedback-message').text('Pleased to meet you, ' + name);
-  });
+  $('#butn-create-item').on('click', function() {
+    let name = $('#input-create-item').val();
+    $('#input-create-item').val('');
 
-  $('a').on('click', function(event) {
-    event.preventDefault();
-    $('#feedback-message').text('That\'s fine!')
+    let html = '';
+    html += '<div class="item">';
+    html += '<div class="name">' + name + '</div>';
+    html += '<img src="assets/image.webp" alt="Image">';
+    html += '<div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quaerat </div>';
+    html += '<div class="price">499</div>';
+    html += '<button>Add to Cart</button>';
+    html += '<br>';
+    html += '<a href="#">More info</a>';
+    html += '<div class="more-info">Lorem ipsum dolor sit amet consectetur, omnis corporis id excepturi </div>';
+    html += '</div>';
+
+    $('#container').prepend(html);
   });
 });
+// You can use $(this).toggleClass('hightlight')
